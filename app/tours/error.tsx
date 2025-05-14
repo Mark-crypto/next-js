fb9ef381b76bf7afca19c0b11b92eaa9df33fd45
Mark-crypto <Mark.Onyango@strathmore.edu>
2025-05-14 19:04:24 +0300

    adding loading and error pages

---
+++ app/tours/error.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+const error = ({ error }: { error: Error }) => {
+  return (
+    <>
+      <h4>There was an error...</h4>
+    </>
+  );
+};
+export default error;
---
+++ app/tours/loading.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+const loading = () => {
+  return (
+    <>
+      <h1 className="text-xl capitalize">Loading Tours ...</h1>
+    </>
+  );
+};
+export default loading;
