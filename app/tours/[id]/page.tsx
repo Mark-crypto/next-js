1352cce5b74da9dbab0550e1843bda794505322c
Mark-crypto <Mark.Onyango@strathmore.edu>
2025-05-21 16:07:59 +0300

    add: Add remote and local images to the project

--- app/tours/[id]/page.tsx
+++ app/tours/[id]/page.tsx
@@ -1,7 +1,35 @@
+import dataImage from "@/images/data.jpg";
+import Image from "next/image";
+
+const url = "https://www.course-api.com/images/tours/tour-1.jpeg";
 const page = ({ params }: { params: { id: string } }) => {
   return (
     <>
       <h1 className="text-4xl">ID: {params.id}</h1>
+      <section className="flex gap-x-4 mt-4">
+        <div>
+          <Image
+            src={dataImage}
+            alt="Data image"
+            width={192}
+            height={192}
+            priority={true}
+            className="w-48 h-48 object-cover rounded"
+          />
+          <h2>Local image</h2>
+        </div>
+        <div>
+          <Image
+            src={url}
+            alt="Remote image"
+            width={192}
+            height={192}
+            priority={true}
+            className="w-48 h-48 object-cover rounded"
+          />
+          <h2>Remote Image</h2>
+        </div>
+      </section>
     </>
   );
 };
